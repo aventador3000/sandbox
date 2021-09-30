90f298fc5942695c40ac84efd8cf8a846978b21c
aventador3000 <50798505+aventador3000@users.noreply.github.com>
2021-09-30 07:55:29 +0300

    Create monads.java

---
+++ monads.java
@@ -0,0 +1,35 @@
+import java.util.function.Function;
+
+interface Monad<T> {
+ <U> Monad<U> bind(Function<T, Monad<U>> f);
+}
+
+class Maybe<T> implements Monad<T> {
+
+ private final T val;
+
+ public Maybe(T val) {
+  this.val = val;
+ }
+
+ public T getVal() {
+  return val;
+ }
+
+ @Override
+ public <U> Monad<U> bind(Function<T, Monad<U>> f) {
+  if (val == null)
+   return new Maybe<U>(null);
+  return f.apply(val);
+ }
+}
+
+public class MonadApp {
+ public static void main(String[] args) {
+  Maybe<Integer> x = new Maybe<>(5);
+  Monad<Integer> y = x
+    .bind(v -> new Maybe<>(v + 1))
+    .bind(v -> new Maybe<>(v * 2));
+  System.out.println( ((Maybe<Integer>)y).getVal() );
+ }
+}
